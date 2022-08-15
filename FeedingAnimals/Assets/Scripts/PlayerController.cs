8849f1cbcaf905fa514d04e76525f7ccfa9f53b7
Ben Chan <ben95chan@gmail.com>
2022-08-15 17:00:51 +0800

    v6

--- FeedingAnimals/Assets/Scripts/PlayerController.cs
+++ FeedingAnimals/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public GameObject projectilePrefab;
 
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +33,25 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+
+        if (transform.position.z >zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
+
+
+
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
 
         //Launch projectile
         if (Input.GetKeyDown(KeyCode.Space))
