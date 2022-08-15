0cb8fc83d97276140bd698fe38264b1150fa384b
Ben Chan <ben95chan@gmail.com>
2022-08-15 15:27:10 +0800

    v3

--- FeedingAnimals/Assets/Scripts/SpawnManager.cs
+++ FeedingAnimals/Assets/Scripts/SpawnManager.cs
@@ -7,24 +7,20 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalsPrefabs;
     private float spawnX = 20;
     private float spawnZ = 20;
+    private float startDelay = 2;
+    private float spawnIntervals = 1.0f;
+
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnIntervals);
     }
 
     // Update is called once per frame
     void Update()
     {
        
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-
-            SpawnRandomAnimal();
-
-        }
     }
   // Spawn Random Animals
     void SpawnRandomAnimal()
